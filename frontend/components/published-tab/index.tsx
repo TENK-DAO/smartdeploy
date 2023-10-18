cfba92fbf7f9825cb098cc63e629a00051213d7a
asanson <alexandresanson.pro@gmail.com>
2023-10-18 15:33:31 +0200

    Published Contract tab design almost finished: cells width ok

--- frontend/components/published-tab/index.tsx
+++ frontend/components/published-tab/index.tsx
@@ -2,92 +2,106 @@ import styles from './style.module.css';
 
 export default function PublishedTab() {
     return(
-        <table className={styles.publishedTab}>
-            <caption>PUBLISHED CONTRACTS</caption>
-            <thead>
-                <tr>
-                    <th>Contract</th>
-                    <th>Author</th>
-                    <th>Version</th>
-                    <th>Hash</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td>Errorszzzzz</td>
-                    <td>k</td>
-                    <td>v0.0.1</td>
-                    <td>Icon</td>
-                </tr>
-                <tr>
-                    <td>Errors</td>
-                    <td>k</td>
-                    <td>v0.0.1</td>
-                    <td>Icon</td>
-                </tr>
-                <tr>
-                    <td>Errors</td>
-                    <td>k</td>
-                    <td>v0.0.1</td>
-                    <td>Icon</td>
-                </tr>
-                <tr>
-                    <td>Errors</td>
-                    <td>k</td>
-                    <td>v0.0.1</td>
-                    <td>Icon</td>
-                </tr>
-                <tr>
-                    <td>Errors</td>
-                    <td>k</td>
-                    <td>v0.0.1</td>
-                    <td>Icon</td>
-                </tr>
-                <tr>
-                    <td>Errors</td>
-                    <td>k</td>
-                    <td>v0.0.1</td>
-                    <td>Icon</td>
-                </tr>
-                <tr>
-                    <td>Errors</td>
-                    <td>k</td>
-                    <td>v0.0.1</td>
-                    <td>Icon</td>
-                </tr>
-                <tr>
-                    <td>Errors</td>
-                    <td>k</td>
-                    <td>v0.0.1</td>
-                    <td>Icon</td>
-                </tr>
-                <tr>
-                    <td>Errors</td>
-                    <td>k</td>
-                    <td>v0.0.1</td>
-                    <td>Icon</td>
-                </tr>
-                <tr>
-                    <td>Errors</td>
-                    <td>k</td>
-                    <td>v0.0.1</td>
-                    <td>Icon</td>
-                </tr>
-                <tr>
-                    <td>Errors</td>
-                    <td>k</td>
-                    <td>v0.0.1</td>
-                    <td>Icon</td>
-                </tr>
-                <tr>
-                    <td>Errors</td>
-                    <td>k</td>
-                    <td>v0.0.1</td>
-                    <td>Icon</td>
-                </tr>
-            </tbody>
+        <div className={styles.publishedTabContainer}>
+            <table className={styles.publishedTabHead}>
+                <caption>PUBLISHED CONTRACTS</caption>
+                <col className={styles.contractCol}></col>
+                <col className={styles.authorCol}></col>
+                <col className={styles.versionCol}></col>
+                <col className={styles.hashCol}></col>
+                <thead>
+                    <tr>
+                        <th>Contract</th>
+                        <th>Author</th>
+                        <th>Version</th>
+                        <th>Hash</th>
+                    </tr>
+                </thead>
+            </table>
+            <div className={styles.publishedTabContentContainer}>
+                <table className={styles.publishedTabContent}>
+                    <col className={styles.contractCol}></col>
+                    <col className={styles.authorCol}></col>
+                    <col className={styles.versionCol}></col>
+                    <col className={styles.hashCol}></col>
+                    <tbody>
+                        <tr>
+                            <td className={styles.contractCell}>Errorzzzzzzzzzzzzzzzzzzzzzzzzs</td>
+                            <td className={styles.authorCell}>CBG572OEPI6LNOUHCOCEUOCDII72UNUWNHCCGB4CHCHB52VQ3RI6NEOWK</td>
+                            <td className={styles.versionCell}>v0.0.105256</td>
+                            <td className={styles.hashCell}>Icon</td>
+                        </tr>
+                        <tr>
+                            <td>vzpeivb</td>
+                            <td>C</td>
+                            <td>v0.0.1</td>
+                            <td>Icon</td>
+                        </tr>
+                        <tr>
+                            <td>Errors</td>
+                            <td>k</td>
+                            <td>v0.0.1</td>
+                            <td>Icon</td>
+                        </tr>
+                        <tr>
+                            <td>Errors</td>
+                            <td>k</td>
+                            <td>v0.0.1</td>
+                            <td>Icon</td>
+                        </tr>
+                        <tr>
+                            <td>Errors</td>
+                            <td>k</td>
+                            <td>v0.0.1</td>
+                            <td>Icon</td>
+                        </tr>
+                        <tr>
+                            <td>Errors</td>
+                            <td>k</td>
+                            <td>v0.0.1</td>
+                            <td>Icon</td>
+                        </tr>
+                        <tr>
+                            <td>Errors</td>
+                            <td>k</td>
+                            <td>v0.0.1</td>
+                            <td>Icon</td>
+                        </tr>
+                        <tr>
+                            <td>Errors</td>
+                            <td>k</td>
+                            <td>v0.0.1</td>
+                            <td>Icon</td>
+                        </tr>
+                        <tr>
+                            <td>Errors</td>
+                            <td>k</td>
+                            <td>v0.0.1</td>
+                            <td>Icon</td>
+                        </tr>
+                        <tr>
+                            <td>Errors</td>
+                            <td>k</td>
+                            <td>v0.0.1</td>
+                            <td>Icon</td>
+                        </tr>
+                        <tr>
+                            <td>Errors</td>
+                            <td>k</td>
+                            <td>v0.0.1</td>
+                            <td>Icon</td>
+                        </tr>
+                        <tr>
+                            <td>Errors</td>
+                            <td>k</td>
+                            <td>v0.0.1</td>
+                            <td>Icon</td>
+                        </tr>
+                    </tbody>
 
-        </table>
+                </table>
+            </div>
+        </div>
         
     )
 }
