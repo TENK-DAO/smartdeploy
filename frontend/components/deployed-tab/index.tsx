0f56320afd01f9f369a7dd31d8dedf536a14b389
asanson <alexandresanson.pro@gmail.com>
2023-10-18 22:55:56 +0200

    Add the Deployed Contracts Table

---
+++ frontend/components/deployed-tab/index.tsx
@@ -0,0 +1,120 @@
+import { FaRegClipboard } from "react-icons/fa";
+import styles from './style.module.css';
+
+export default function DeployedTab() {
+    return(
+        <div className={styles.deployedTabContainer}>
+            <table className={styles.deployedTabHead}>
+                <caption>DEPLOYED CONTRACTS</caption>
+                <colgroup>
+                    <col className={styles.contractCol}></col>
+                    <col className={styles.addressCol}></col>
+                    <col className={styles.copyCol}></col>
+                </colgroup>
+                <thead>
+                    <tr>
+                        <th>Contract</th>
+                        <th>Address</th>
+                        <th>Copy</th>
+                    </tr>
+                </thead>
+            </table>
+            <div className={styles.deployedTabContentContainer}>
+                <table className={styles.deployedTabContent}>
+                    <colgroup>
+                        <col className={styles.contractCol}></col>
+                        <col className={styles.addressCol}></col>
+                        <col className={styles.copyCol}></col>
+                    </colgroup>
+                    <tbody>
+                        <tr>
+                            <td className={styles.contractCell}>Errorrrrrrrrrrrrrrrrrrrrrrrr</td>
+                            <td>CBG572OEPI6LNOUHCOCEUOCDII72UNUWNHCCGB4CHCHB52VQ3RI6NEOWK</td>
+                            <td className={styles.clipboardIcon}><FaRegClipboard/></td>
+                        </tr>
+                        <tr>
+                            <td className={styles.contractCell}>Smart_Deploy</td>
+                            <td>CBG572OEPI6LNOUHCOCEUOCDII72UNUWNHCCGB4CHCHB52VQ3RI6NEOWK</td>
+                            <td className={styles.clipboardIcon}><FaRegClipboard/></td>
+                        </tr>
+
+                        <tr>
+                            <td className={styles.contractCell}>Errorrrrrrrrr</td>
+                            <td>CBG572OEPI6LNOUHCOCEUOCDII72UNUWNHCCGB4CHCHB52VQ3RI6NEOWK</td>
+                            <td className={styles.clipboardIcon}><FaRegClipboard/></td>
+                        </tr>
+                        <tr>
+                            <td className={styles.contractCell}>Err</td>
+                            <td>CBG572OEPI6LNOUHCOCEUOCDII72UNUWNHCCGB4CHCHB52VQ3RI6NEOWK</td>
+                            <td className={styles.clipboardIcon}><FaRegClipboard/></td>
+                        </tr>
+
+                        <tr>
+                            <td className={styles.contractCell}>Errorrrr</td>
+                            <td>CBG572OEPI6LNOUHCOCEUOCDII72UNUWNHCCGB4CHCHB52VQ3RI6NEOWK</td>
+                            <td className={styles.clipboardIcon}><FaRegClipboard/></td>
+                        </tr>
+                        <tr>
+                            <td className={styles.contractCell}>Errorrrrrrrr</td>
+                            <td>CBG572OEPI6LNOUHCOCEUOCDII72UNUWNHCCGB4CHCHB52VQ3RI6NEOWK</td>
+                            <td className={styles.clipboardIcon}><FaRegClipboard/></td>
+                        </tr>
+
+                        <tr>
+                            <td className={styles.contractCell}>zeobv</td>
+                            <td>CBG572OEPI6LNOUHCOCEUOCDII72UNUWNHCCGB4CHCHB52VQ3RI6NEOWK</td>
+                            <td className={styles.clipboardIcon}><FaRegClipboard/></td>
+                        </tr>
+                        <tr>
+                            <td className={styles.contractCell}>Errorrrrrrrrrrrrrrrrrrrrrrrr</td>
+                            <td>CBG572OEPI6LNOUHCOCEUOCDII72UNUWNHCCGB4CHCHB52VQ3RI6NEOWK</td>
+                            <td className={styles.clipboardIcon}><FaRegClipboard/></td>
+                        </tr>
+                        <tr>
+                            <td className={styles.contractCell}>Errorrrr</td>
+                            <td>CBG572OEPI6LNOUHCOCEUOCDII72UNUWNHCCGB4CHCHB52VQ3RI6NEOWK</td>
+                            <td className={styles.clipboardIcon}><FaRegClipboard/></td>
+                        </tr>
+                        <tr>
+                            <td className={styles.contractCell}>Errorrrrrrrr</td>
+                            <td>CBG572OEPI6LNOUHCOCEUOCDII72UNUWNHCCGB4CHCHB52VQ3RI6NEOWK</td>
+                            <td className={styles.clipboardIcon}><FaRegClipboard/></td>
+                        </tr>
+
+                        <tr>
+                            <td className={styles.contractCell}>zeobv</td>
+                            <td>CBG572OEPI6LNOUHCOCEUOCDII72UNUWNHCCGB4CHCHB52VQ3RI6NEOWK</td>
+                            <td className={styles.clipboardIcon}><FaRegClipboard/></td>
+                        </tr>
+                        <tr>
+                            <td className={styles.contractCell}>Errorrrrrrrrrrrrrrrrrrrrrrrr</td>
+                            <td>CBG572OEPI6LNOUHCOCEUOCDII72UNUWNHCCGB4CHCHB52VQ3RI6NEOWK</td>
+                            <td className={styles.clipboardIcon}><FaRegClipboard/></td>
+                        </tr>
+                        <tr>
+                            <td className={styles.contractCell}>Errorrrr</td>
+                            <td>CBG572OEPI6LNOUHCOCEUOCDII72UNUWNHCCGB4CHCHB52VQ3RI6NEOWK</td>
+                            <td className={styles.clipboardIcon}><FaRegClipboard/></td>
+                        </tr>
+                        <tr>
+                            <td className={styles.contractCell}>Errorrrrrrrr</td>
+                            <td>CBG572OEPI6LNOUHCOCEUOCDII72UNUWNHCCGB4CHCHB52VQ3RI6NEOWK</td>
+                            <td className={styles.clipboardIcon}><FaRegClipboard/></td>
+                        </tr>
+
+                        <tr>
+                            <td className={styles.contractCell}>zeobv</td>
+                            <td>CBG572OEPI6LNOUHCOCEUOCDII72UNUWNHCCGB4CHCHB52VQ3RI6NEOWK</td>
+                            <td className={styles.clipboardIcon}><FaRegClipboard/></td>
+                        </tr>
+                        <tr>
+                            <td className={styles.contractCell}>Errorrrrrrrrrrrrrrrrrrrrrrrr</td>
+                            <td>CBG572OEPI6LNOUHCOCEUOCDII72UNUWNHCCGB4CHCHB52VQ3RI6NEOWK</td>
+                            <td className={styles.clipboardIcon}><FaRegClipboard/></td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    )
+}
--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -4,6 +4,7 @@ import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
 import WalletInfo from '@/components/wallet'
 import PublishedTab from '@/components/published-tab'
+import DeployedTab from '@/components/deployed-tab'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -33,6 +34,7 @@ export default function Home() {
         </div>
 
         <PublishedTab/>
+        <DeployedTab/>
 
         <div className={styles.grid}>
           <a
@@ -91,184 +93,7 @@ export default function Home() {
               with&nbsp;Vercel.
             </p>
           </a>
-        </div>
-
-        <div className={styles.grid}>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>
-              Docs <span>-&gt;</span>
-            </h2>
-            <p>
-              Find in-depth information about Next.js features and&nbsp;API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>
-              Learn <span>-&gt;</span>
-            </h2>
-            <p>
-              Learn about Next.js in an interactive course with&nbsp;quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>
-              Templates <span>-&gt;</span>
-            </h2>
-            <p>
-              Discover and deploy boilerplate example Next.js&nbsp;projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>
-              Deploy <span>-&gt;</span>
-            </h2>
-            <p>
-              Instantly deploy your Next.js site to a shareable URL
-              with&nbsp;Vercel.
-            </p>
-          </a>
-        </div>
-
-        <div className={styles.grid}>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>
-              Docs <span>-&gt;</span>
-            </h2>
-            <p>
-              Find in-depth information about Next.js features and&nbsp;API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>
-              Learn <span>-&gt;</span>
-            </h2>
-            <p>
-              Learn about Next.js in an interactive course with&nbsp;quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>
-              Templates <span>-&gt;</span>
-            </h2>
-            <p>
-              Discover and deploy boilerplate example Next.js&nbsp;projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>
-              Deploy <span>-&gt;</span>
-            </h2>
-            <p>
-              Instantly deploy your Next.js site to a shareable URL
-              with&nbsp;Vercel.
-            </p>
-          </a>
-        </div>
-
-        <div className={styles.grid}>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>
-              Docs <span>-&gt;</span>
-            </h2>
-            <p>
-              Find in-depth information about Next.js features and&nbsp;API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>
-              Learn <span>-&gt;</span>
-            </h2>
-            <p>
-              Learn about Next.js in an interactive course with&nbsp;quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>
-              Templates <span>-&gt;</span>
-            </h2>
-            <p>
-              Discover and deploy boilerplate example Next.js&nbsp;projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>
-              Deploy <span>-&gt;</span>
-            </h2>
-            <p>
-              Instantly deploy your Next.js site to a shareable URL
-              with&nbsp;Vercel.
-            </p>
-          </a>
-        </div>
+        </div>        
 
         <div className={styles.tenkLogo}>
           <a
