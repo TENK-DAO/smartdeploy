2397d947e406e1075fcc2fb46d2e0235d26f356e
Willem Wyndham <willem@pop-os.tailae3f2.ts.net>
2024-01-08 21:49:45 -0500

    feat!: Redeploy smartdeploy on testnet

--- examples/cross_contract/contract_c/src/lib.rs
+++ examples/cross_contract/contract_c/src/lib.rs
@@ -1,7 +1,7 @@
 #![no_std]
 use loam_sdk::soroban_sdk::{self, contract, contractimpl, Address, Env};
 
-smartdeploy_sdk::import_contract!(contract_a);
+loam_sdk::import_contract!(contract_a);
 
 #[contract]
 pub struct ContractB;
@@ -9,7 +9,7 @@ pub struct ContractB;
 #[contractimpl]
 impl ContractB {
     pub fn add_with(env: Env, contract_id: Address, x: u32, y: u32) -> u32 {
-        let client = contract_a::new(&env);
+        let client = contract_a::Client::new(&env, &contract_id);
         let client_2 = contract_a::Client::new(&env, &contract_id);
         (client.add(&x, &y) + client_2.add(&x, &y)) >> 2
     }
